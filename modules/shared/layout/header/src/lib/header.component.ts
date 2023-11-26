97c1b042e95e0a8e68d5a0d2c8fa308c1079aed7
tdp99 <tdp99.business@gmail.com>
2023-11-26 15:47:15 +0700

    add github pages deployment

--- modules/shared/layout/header/src/lib/header.component.ts
+++ modules/shared/layout/header/src/lib/header.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 
 @Component({
     selector: 'fm-header',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CdkMenuTrigger, CdkMenu, CdkMenuItem, RouterModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
